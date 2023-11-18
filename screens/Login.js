545997bda0a7795400496698039699c7a91e2d90
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2023-11-18 12:55:55 +0000

    loginscreen navigation to homescreen

--- screens/Login.js
+++ screens/Login.js
@@ -2,19 +2,19 @@ import { View, Text, SafeAreaView, Image, TouchableOpacity,StyleSheet ,KeyboardA
 import React from 'react'
 import { TextInput } from 'react-native-gesture-handler'
 
-const Login = () => {
+const Login = ({navigation}) => {
   return (
     <SafeAreaView style={{flex:1,alignItems:"center",display:"flex",justifyContent:"space-evenly"}}>
         <View style={{display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"center",width:"70%",height:"40%",marginTop:"5%"}}>
             <Image style={{}}  source={require('../assets/vvulogo.png')}/>
-            <Text style={{fontSize:20,fontFamily:'sans-ser'}}>VVU MARKET</Text>       
+            <Text style={{fontSize:20}}>VVU MARKET</Text>       
             <Text>Best Campus Shopping Experience</Text>
         </View>
 
         <KeyboardAvoidingView style={{display:"flex",flexDirection:"column",height:"40%",width:"90%",marginTop:"3%",justifyContent:"space-evenly"}}>
             <TextInput style={styles.Input} placeholder='Enter your Id'/>
             <TextInput placeholder='Enter Password'/>
-            <TouchableOpacity style={{alignItems:"center",justifyContent:"center",backgroundColor:"purple",height:'20%',borderRadius:5}}>
+            <TouchableOpacity onPress={()=>navigation.navigate('Tabs')} style={{alignItems:"center",justifyContent:"center",backgroundColor:"purple",height:'20%',borderRadius:5}}>
                 <Text style={{color:"white",fontSize:18}}>Login</Text>
             </TouchableOpacity>
         </KeyboardAvoidingView>
