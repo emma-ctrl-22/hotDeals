f60685fbeb9396e91adc765a51083cd3a2753594
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2023-12-12 01:06:46 +0000

    Add email and password state variables and login functionality

--- screens/Login.js
+++ screens/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {
   View,
   Text,
@@ -12,6 +12,26 @@ import {
 } from 'react-native';
 
 const Login = ({ navigation }) => {
+  const [email, setemail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post('http://localhost:3000/login', {
+        email,
+        password,
+      });
+
+      if (response.data.success) {
+        // Navigate to the homepage or perform any desired action upon successful login
+        navigation.navigate('Tabs');
+      } else {
+        console.log('Login failed');
+      }
+    } catch (error) {
+      console.error('An error occurred during login:', error.message);
+    }
+  };
   return (
     <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'space-evenly' }}>
       <View style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '70%', height: '20%', marginTop: '5%' }}>
@@ -22,9 +42,11 @@ const Login = ({ navigation }) => {
 
       <KeyboardAvoidingView
         style={{ display: 'flex', flexDirection: 'column', height: '40%', width: '90%', marginTop: '3%', justifyContent: 'space-evenly',alignItems:"center" }}>
-        <TextInput style={styles.input} placeholder="Enter your Id" />
-        <TextInput style={styles.input} placeholder="Enter Password" secureTextEntry={true} />
-        <TouchableOpacity onPress={() => navigation.navigate('Tabs')} style={{ alignItems: 'center', justifyContent: 'center', backgroundColor: 'purple', height: '22%', borderRadius: 5 ,width:"98%"}}>
+        <TextInput style={styles.input} placeholder="Enter your Id" value={email}
+        onChangeText={(text) => setemail(text)} />
+        <TextInput style={styles.input} placeholder="Enter Password" secureTextEntry={true} value={password}
+        onChangeText={(text) => setPassword(text)} />
+        <TouchableOpacity onPress={handleSubmit} style={{ alignItems: 'center', justifyContent: 'center', backgroundColor: 'purple', height: '22%', borderRadius: 5 ,width:"98%"}}>
           <Text style={{ color: 'white', fontSize: 18 }}>Login</Text>
         </TouchableOpacity>
       </KeyboardAvoidingView>
