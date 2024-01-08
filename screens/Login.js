08da694c474b45c5a34622599870c8b0e127f0f8
Danielkojo <danielkojo005@gmail.com>
2024-01-08 16:16:57 +0000

    added

--- screens/Login.js
+++ screens/Login.js
@@ -1,4 +1,5 @@
 //start
+import { useNavigation } from '@react-navigation/native';
 import React,{useState} from 'react';
 import {
   View,
@@ -14,23 +15,36 @@ import {
 import axios from 'axios';
 
 const Login = ({ navigation }) => {
-  const [email, setemail] = useState('');
-  const [password, setPassword] = useState('');
+  const[email,setEmail]=useState('');
+  const[password,setPassword]=useState('');
+  const [loading, setLoading] = useState(false);
+  const navigation = useNavigation()
 
   const handleSubmit = async () => {
-    try {
-      const response = await axios.post('http://154.160.7.229:3000/login', {
-        email,
-        password,
-      },{timeout: 100000});
-
-      if (res.data.Status === "Success"){
-        console.log('success')
-        navigation.navigate('Tabs');
+     
+  try {
+    setLoading(true);
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+    // Assuming you have a collection named 'users' in Firestore
+    const userDocRef = doc(db, 'users', user.uid);
+    const userDoc = await getDoc(userDocRef);
+    
+    if (userDoc.exists()) {
+      const userData = userDoc.data();
+      if (userData.role === 'admin') {
+    
+        navigation.replace("AdminTabs")
+      } else {
+       
+        navigation.replace("ShopScreen")
       }
-    } catch (error) {
-      console.error('An error occurred during login:', error.message);
     }
+  }
+catch (err) {
+  console.error(err);
+  Alert.alert('Error', err.message);// Set the error message in state
+}
   };
   return (
     <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'space-evenly' }}>
@@ -43,7 +57,7 @@ const Login = ({ navigation }) => {
       <KeyboardAvoidingView
         style={{ display: 'flex', flexDirection: 'column', height: '40%', width: '90%', marginTop: '3%', justifyContent: 'space-evenly',alignItems:"center" }}>
         <TextInput style={styles.input} placeholder="Enter your Id" value={email}
-        onChangeText={(text) => setemail(text)} />
+        onChangeText={(text) => setEmail(text)} />
         <TextInput style={styles.input} placeholder="Enter " secureTextEntry={true} value={password}
         onChangeText={(text) => setPassword(text)} />
         <TouchableOpacity onPress={handleSubmit} style={{ alignItems: 'center', justifyContent: 'center', backgroundColor: 'purple', height: '22%', borderRadius: 5 ,width:"98%"}}>
